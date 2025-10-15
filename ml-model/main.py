a257370f3061bed44b28a1deb645b9fba0e88c3f
McDonaghMichael <G00421944@atu.ie>
2025-10-15 15:24:12 +0100

    Began work on trianing the ML model on multiple inputs, in order to get multiple outputs

--- ml-model/main.py
+++ ml-model/main.py
@@ -3,27 +3,30 @@ import torch
 import torch.nn as nn
 import sklearn
 import openpyxl
-print(openpyxl.__version__)
-
-
 from sklearn.preprocessing import StandardScaler
 
+# Open up and load the data from the excel file, in the future this will be training from data from the database!!!
 df = pd.read_excel('data.xlsx')
 
-x = df[['memory_usage']].values
+input_values = ['memory_usage', 'disk_usage']
+output_values= ['cpu_usage', 'disk_usage']
 
-y = df[['cpu_usage']].values.reshape(-1, 1)
+x = df[input_values].values
+y = df[output_values].values
 
-scaler = StandardScaler()
-x_scaled = scaler.fit_transform(x)
-
+x_scaler = StandardScaler()
+y_scaler = StandardScaler()
+x_scaled = x_scaler.fit_transform(x)
+y_scaled = y_scaler.fit_transform(y)
 
 x_tensor = torch.tensor(x_scaled, dtype=torch.float32)
-y_tensor = torch.tensor(y, dtype=torch.float32)
+y_tensor = torch.tensor(y_scaled, dtype=torch.float32)
 
 
 model = nn.Sequential(
-    nn.Linear(1, 1),
+    nn.Linear(2,4),
+    nn.ReLU(),
+    nn.Linear(4,2)
 )
 
 criterion = nn.MSELoss()  
@@ -39,7 +42,12 @@ for epoch in range(2000):
     if (epoch + 1) % 100 == 0:
         print(f'Epoch {epoch+1}, Loss: {loss.item():.4f}')
 
-test_memory_usage = torch.tensor([[60.0]], dtype=torch.float32)
-test_memory_usage_scaled = torch.tensor(scaler.transform([[60.0]]), dtype=torch.float32)
-pred_cpu = model(test_memory_usage_scaled)
-print("Predicted CPU usage for memory_usage=60:", pred_cpu.item())
+test_input = torch.tensor([[60.0, 20.5]], dtype=torch.float32)
+test_scaled = torch.tensor(x_scaler.transform([[60.0, 20.5]]), dtype=torch.float32)
+pred = model(test_scaled)
+predictions = y_scaler.inverse_transform(pred.detach().numpy())
+
+
+print("Predicted CPU usage for memory_usage=60:")
+print(f"Predicted CPU usage: {predictions[0][0]:.2f}%")
+print(f"Predicted Disk usage: {predictions[0][1]:.2f}%")
