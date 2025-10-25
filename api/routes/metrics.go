21d0ca557ce489baa70f1a1e4348ec3e6c129e8d
McDonaghMichael <G00421944@atu.ie>
2025-10-25 23:19:32 +0100

    Ip is now string

--- api/routes/metrics.go
+++ api/routes/metrics.go
@@ -1,9 +1,9 @@
 package routes
 
 type Statistics struct {
-	ID                int `json:"id"`
-	IPAddress         int `json:"ip_address`
-	NumOfCPU          int `json:"num_of_cpu"`
-	MemoryAllocated   int `json:"memory_allocated"`
-	MemoryAllocations int `json:"memory_allocations"`
+	ID                int    `json:"id"`
+	IPAddress         string `json:"ip_address`
+	NumOfCPU          int    `json:"num_of_cpu"`
+	MemoryAllocated   int    `json:"memory_allocated"`
+	MemoryAllocations int    `json:"memory_allocations"`
 }
