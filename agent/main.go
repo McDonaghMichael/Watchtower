3895abb632b07c9520ccd8f02c2c2ab7e8f1d6b7
McDonaghMichael <G00421944@atu.ie>
2025-10-25 23:53:40 +0100

    Added forever loop with sleep of 5s in between metric posts

--- agent/main.go
+++ agent/main.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"net/http"
 	"runtime"
+	"time"
 )
 
 type Metrics struct {
@@ -18,48 +19,55 @@ type Metrics struct {
 
 func main() {
 
-	var mem runtime.MemStats
-	runtime.ReadMemStats(&mem)
+	for {
+		var mem runtime.MemStats
+		runtime.ReadMemStats(&mem)
 
-	var numOfCPU int = runtime.NumCPU()
-	var totalMemoryAllocated uint64 = mem.Alloc
-	var totalMemoryAllocations uint64 = mem.Mallocs
+		var numOfCPU int = runtime.NumCPU()
+		var totalMemoryAllocated uint64 = mem.Alloc
+		var totalMemoryAllocations uint64 = mem.Mallocs
 
-	var routeIP string = "http://localhost:8080/api/v1/metric"
-	ipAddress := "80.209.228.5"
-	jsonString := fmt.Sprintf(`{"ip_address": "%s", "num_of_cpu": %v, "memory_allocated": %v, "memory_allocations": %v}`,
-		ipAddress, numOfCPU, totalMemoryAllocated, totalMemoryAllocations)
-	body := []byte(jsonString)
+		var routeIP string = "http://localhost:8080/api/v1/metric"
 
-	r, err := http.NewRequest("POST", routeIP, bytes.NewBuffer(body))
+		ipAddress := "80.209.228.5"
 
-	if err != nil {
-		panic(err)
+		jsonString := fmt.Sprintf(`{"ip_address": "%s", "num_of_cpu": %v, "memory_allocated": %v, "memory_allocations": %v}`,
+			ipAddress, numOfCPU, totalMemoryAllocated, totalMemoryAllocations)
+		body := []byte(jsonString)
+
+		r, err := http.NewRequest("POST", routeIP, bytes.NewBuffer(body))
+
+		if err != nil {
+			panic(err)
+		}
+
+		r.Header.Add("Content-Type", "application/json")
+
+		client := &http.Client{}
+		res, err := client.Do(r)
+		if err != nil {
+			panic(err)
+		}
+
+		defer res.Body.Close()
+
+		post := &Metrics{}
+		derr := json.NewDecoder(res.Body).Decode(post)
+		if derr != nil {
+			panic(derr)
+		}
+
+		if res.StatusCode != http.StatusCreated {
+			panic(res.Status)
+		}
+
+		fmt.Println("Id:", post.ID)
+		fmt.Println("IP:", post.IPAddress)
+		fmt.Println("Num Of CPU:", post.NumOfCPU)
+		fmt.Println("Memory Allocated:", post.MemoryAllocated)
+		fmt.Println("Mmeory Allocations:", post.MemoryAllocations)
+
+		time.Sleep(time.Second * 5)
 	}
 
-	r.Header.Add("Content-Type", "application/json")
-
-	client := &http.Client{}
-	res, err := client.Do(r)
-	if err != nil {
-		panic(err)
-	}
-
-	defer res.Body.Close()
-
-	post := &Metrics{}
-	derr := json.NewDecoder(res.Body).Decode(post)
-	if derr != nil {
-		panic(derr)
-	}
-
-	if res.StatusCode != http.StatusCreated {
-		panic(res.Status)
-	}
-
-	fmt.Println("Id:", post.ID)
-	fmt.Println("IP:", post.IPAddress)
-	fmt.Println("Num Of CPU:", post.NumOfCPU)
-	fmt.Println("Memory Allocated:", post.MemoryAllocated)
-	fmt.Println("Mmeory Allocations:", post.MemoryAllocations)
 }
