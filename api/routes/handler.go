a24d41be51b2b079e77a7b8cc7438c54ac959909
McDonaghMichael <G00421944@atu.ie>
2025-11-03 18:14:59 +0000

    fixed

--- api/routes/handler.go
+++ api/routes/handler.go
@@ -55,11 +55,6 @@ func corsMiddleware() gin.HandlerFunc {
 		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
 
-		if c.Request.Method == "OPTIONS" {
-			c.AbortWithStatus(http.StatusNoContent)
-			return
-		}
-
 		c.Next()
 	}
 }
