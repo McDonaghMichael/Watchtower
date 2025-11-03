20f9310fea1ca03bbe03ab4316e78fcaae6e4d12
McDonaghMichael <G00421944@atu.ie>
2025-11-03 18:39:06 +0000

    fixed cors error

--- api/main.go
+++ api/main.go
@@ -15,16 +15,29 @@ func main() {
 	r := gin.Default()
 
 	r.Use(func(c *gin.Context) {
-		c.Writer.Header().Set("Access-Control-Allow-Origin", "http://localhost:3000")
+		allowedOrigins := map[string]bool{
+			"http://localhost:3000":     true,
+			"http://localhost:3001":     true,
+			"http://80.208.227.58:3000": true,
+			"http://80.208.227.58:3001": true,
+		}
+
+		origin := c.Request.Header.Get("Origin")
+
+		if allowedOrigins[origin] {
+			c.Writer.Header().Set("Access-Control-Allow-Origin", origin)
+		}
+
 		c.Writer.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 		c.Writer.Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Writer.Header().Set("Access-Control-Allow-Credentials", "true")
+
 		if c.Request.Method == "OPTIONS" {
 			c.AbortWithStatus(204)
 			return
 		}
 		c.Next()
 	})
-
 	routes.SetupAPIRoutes(r.Group("/api/v1"))
 
 	// Start the ping loop BEFORE starting the server
