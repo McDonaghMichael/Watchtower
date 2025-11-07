a938eb4104d29e17ca536efe3809a21590269261
McDonaghMichael <G00421944@atu.ie>
2025-11-07 13:19:31 +0000

    Created time util class for storing time related utilities

---
+++ web/src/utils/timeUtils.js
@@ -0,0 +1,12 @@
+export const getTimestamp = () => {
+    const now = new Date();
+    return now.toLocaleString('en-UK', { 
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+      hour: '2-digit',
+      minute: '2-digit',
+      second: '2-digit',
+      hour12: false 
+    });
+  };
